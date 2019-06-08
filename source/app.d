d122330473a46635fd2265dbbd3e7d57a9aff8b2
Michael Fadely <michaelwfadely@gmail.com>
2019-06-08 13:36:50 -0600

    this stuff is super hard coded and will be replaced with unit tests

--- source/app.d
+++ source/app.d
@@ -24,12 +24,59 @@ int main(string[] args)
 	}
 
 	auto generator = new ManifestGenerator();
-	auto manifest = generator.Generate(inPath);
+	auto manA = Manifest.fromFile(buildNormalizedPath(inPath, "a.manifest"));
+	auto manB = generator.generate(inPath);
+	auto diff = generator.diff(manB, manA);
+
+	foreach (entry; diff.filter!(x => x.state != ManifestState.unchanged))
+	{
+		auto last = entry.last;
+		auto current = entry.current;
+
+		if (last !is null && current !is null &&
+		    last.filePath != current.filePath)
+		{
+			writeln(entry.last.filePath, " -> ", entry.current.filePath, ": ", entry.state);
+		}
+		else
+		{
+			auto e = last is null ? current : last;
+			writeln(e.filePath, ": ", entry.state);
+		}
+	}
+
+	/*
+	auto generator = new ManifestGenerator();
+	auto manifest = generator.generate(inPath);
 
 	foreach (entry; manifest)
 	{
 		stdout.writeln(entry);
 	}
+	*/
+
+	/*
+
+	auto diff = generator.verify(inPath, manifest);
+	auto modifications = diff.filter!(x => x.state != ManifestState.unchanged);
+
+	foreach (entry; modifications)
+	{
+		auto last = entry.last;
+		auto current = entry.current;
+
+		if (last !is null && current !is null && last.filePath != current.filePath)
+		{
+			writeln(entry.last.filePath, " -> ", entry.current.filePath, ": ", entry.state);
+		}
+		else
+		{
+			auto e = last is null ? current : last;
+			writeln(e.filePath, ": ", entry.state);
+		}
+	}
+
+	*/
 
 	return 0;
 }
