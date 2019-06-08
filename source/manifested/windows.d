5e873e8c1d6b526c105b6e776032ea2e0607e340
Michael Fadely <michaelwfadely@gmail.com>
2019-06-08 12:03:03 -0600

    fix CreateFile

--- source/manifested/windows.d
+++ source/manifested/windows.d
@@ -18,6 +18,12 @@ public:
 		errorCode = GetLastError();
 		super("Operation failed with error code " ~ to!string(errorCode));
 	}
+
+	this(string msg)
+	{
+		errorCode = GetLastError();
+		super("[" ~ to!string(errorCode) ~ "] " ~ msg);
+	}
 }
 
 extern (Windows) nothrow @nogc
@@ -32,14 +38,15 @@ string getFinalPathName(string path)
 {
 	import std.string : toStringz;
 
-	auto handle = CreateFileA(path.toStringz(), FILE_READ_EA, GENERIC_READ | GENERIC_WRITE | DELETE, null,
+	auto strz = path.toStringz();
+	auto handle = CreateFileA(strz, FILE_READ_EA, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, null,
 	                          OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, null);
 
-	scope (exit) CloseHandle(handle);
+	scope (exit) { CloseHandle(handle); }
 
 	if (handle == INVALID_HANDLE_VALUE)
 	{
-		throw new Win32Exception();
+		throw new Win32Exception("Unable to open file " ~ path);
 	}
 
 	auto sb = new char[1024];
@@ -47,7 +54,7 @@ string getFinalPathName(string path)
 
 	if (result == 0)
 	{
-		throw new Win32Exception();
+		throw new Win32Exception("GetFinalPathNameByHandleA failed");
 	}
 
 	return to!string(sb);
